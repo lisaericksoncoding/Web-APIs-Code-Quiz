f9167cbd079fe411fa9543c5c1ecfef19d917b8b
lisaericksoncoding <111710705+lisaericksoncoding@users.noreply.github.com>
2022-09-26 23:02:20 -0500

    revisions to html & css plus inital build of js

---
+++ script.js
@@ -0,0 +1,50 @@
+var startBtn = document.querySelector("#start");
+var timerEl = document.querySelector("#time");
+var questionsEl = document.querySelector("#questions");
+var choicesEl = document.querySelector("#choices");
+var submitBtn = document.querySelector("#submit");
+var initialsEl = document.querySelector("#initials");
+
+var totalQuestions = [
+    {
+        question: "What is the type of Pop up boxes available in JavaScript?:",
+        choices: ["Alert", "Confirm", "Prompt", "All of the above"],
+        answer: "All of the above"
+
+    },
+    {
+        question: "What does the <p> tag stand for?",
+        choices: ["Popup", "Paragraph", "Picture", "None of the above"],
+        answer: "Paragraph"
+    },
+    {
+        question: "What tag is used to define an unordered list that is bulleted?",
+        choices: ["<li>", "<u>", "<ul>", "<lu>"],
+        answer: "<ul>"
+    },
+    {
+        question: "In JavaScript, what element is used to store multiple values in a single variable?",
+        choices: ["Arrays", "Strings", "Functions", "Variables"],
+        answer: "Arrays"
+    },
+    {
+        question: "What is considered to be the most popular programming language in the world?",
+        choices: ["HTML","C++","Ruby","JavaScript" ],
+        answer: "JavaScript"
+    },
+
+];
+startBtn.addEventListener("click", startQuiz())
+
+function startQuiz() {
+    timerEl = setInterval(countdown, 1000);
+    
+}
+function countdown() {
+    time--;
+    timerEl.textContent = time;
+
+    if (time <= 0) {
+      endQuiz();
+    };
+}
